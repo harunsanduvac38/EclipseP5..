9beb67333abf2ba947807359827126439ed38069
mananas <mananas@10.1.2.15>
2025-07-08 13:52:40 +0200

    s17

--- j05_recursividad/src/string/S17FraseCaracter.java
+++ j05_recursividad/src/string/S17FraseCaracter.java
@@ -1,11 +1,18 @@
 package string;
 
+import java.util.ArrayList;
+
 public class S17FraseCaracter {
 	
 	
 	public static int posCaracter(String frase, String caracter) {
+		ArrayList<Integer> posiciones = new ArrayList<>();
+		
 		
 		for(int i = 0; i<frase.length();i++) {
+			
+		}
+		return 5;
 	}
 
 }
